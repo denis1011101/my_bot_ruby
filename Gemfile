751b046742c5acf842fd90f27b8512b1f50c57a4
denis <denisdenis9331@gmail.com>
2022-05-27 01:17:38 +0500

    add telegram

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Gemfile
+if Gem::Version.new(Bundler::VERSION) < Gem::Version.new('2.2.21')
+  abort "Bundler version >= 2.2.21 is required"
+end
+
+gem "telegram-bot-ruby"
+
+ruby "2.4.7"
+
--- myBotRuby.rb
+++ myBotRuby.rb
@@ -1,7 +1,10 @@
-FILE = "unknownEnglishWords".freeze
+require 'telegram/bot'
+
+TOKEN = ENV['TOKEN']
+FILE = 'unknownEnglishWords'.freeze
 
 def read_file
-  file = File.open(FILE, "r")
+  file = File.open(FILE, 'r')
   file_data = file.readlines.map(&:chomp)
   file_data = file_data.reject { |c| c.empty? }
   file.close
@@ -15,8 +18,22 @@ def shuffle_and_show_ten_words
   ten_shuffle_words_without_head = shuffle_words_without_head[0..9]
   ten_shuffle_words_with_head = ten_shuffle_words_without_head.unshift(head_by_file)
   ten_shuffle_words_with_head = ten_shuffle_words_with_head.flat_map { |x| [x, ""] }.tap(&:pop)
-  puts ten_shuffle_words_with_head
+  
+  ten_shuffle_words_with_head
 end
 
-shuffle_and_show_ten_words
+Telegram::Bot::Client.run(TOKEN, logger: Logger.new($stderr)) do |bot|
+  bot.logger.info('Bot has been started')
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, ")
+    when 'auth'
+      bot.api.send_message(chat_id: message.chat.id, text: "let's go")
+      bot.api.send_message(chat_id: message.chat.id, text: "#{shuffle_and_show_ten_words}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
 
