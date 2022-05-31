f12f1a730c5de8274e8f9bfc8dfebb596decd2fc
denis <denisdenis9331@gmail.com>
2022-05-31 21:26:51 +0500

    add word, help and refactoring

--- my_bot_ruby.rb
+++ my_bot_ruby.rb
@@ -14,18 +14,17 @@ def read_file
   file_data
 end
 
-def shuffle_and_show_some_words(count_words = 3)
+def shuffle_some_words(count_words = 3)
   count_words -= 1
   head_by_file = read_file.first
   shuffle_words_without_head = read_file[1..].shuffle
   some_shuffle_words_ex_head = shuffle_words_without_head[0..count_words]
-  some_shuffle_words_with_head = some_shuffle_words_ex_head.unshift(head_by_file)
-  some_shuffle_words_with_head.flat_map { |x| [x, ''] }.tap(&:pop)
+  some_shuffle_words_ex_head.unshift(head_by_file)
 end
 
-def format_message(some_words)
-  some_words[0] = "*#{some_words[0]}*"
-  some_words.map { |x| "#{x}\n" }.join
+def format_message(some_words, flag: false)
+  some_words[0] = "*#{some_words[0]}*" if flag == true
+  some_words.flat_map { |x| [x, ''] }.tap(&:pop).join("\n")
 end
 
 def send_telegram_message(message, chat_id = '85611094')
@@ -34,7 +33,11 @@ def send_telegram_message(message, chat_id = '85611094')
               { chat_id: chat_id, text: message, parse_mode: 'Markdown' })
 end
 
-send_telegram_message(format_message(shuffle_and_show_some_words))
+def help
+  %w[/start /words /stop]
+end
+
+send_telegram_message(format_message(shuffle_some_words, flag: true))
 
 Telegram::Bot::Client.run(TOKEN, logger: Logger.new($stderr)) do |bot|
   bot.logger.info('Bot has been started')
@@ -42,9 +45,11 @@ Telegram::Bot::Client.run(TOKEN, logger: Logger.new($stderr)) do |bot|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, ")
-    when 'words'
-      bot.api.send_message(chat_id: message.chat.id, text: format_message(shuffle_and_show_some_words),
+    when '/words'
+      bot.api.send_message(chat_id: message.chat.id, text: format_message(shuffle_some_words, flag: true),
                            parse_mode: 'Markdown')
+    when '/help'
+      bot.api.send_message(chat_id: message.chat.id, text: format_message(help))
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     else
