a396413dfa2b16d6338eaab05ec317c5fef2424c
denis <denisdenis9331@gmail.com>
2022-11-28 01:00:13 +0500

    add export token into script

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ killall -9 ruby my_bot_ruby.rb
 
 rm -rf my_bot_ruby
 
+export TOKEN=5433802293:AAEFGH67GVCC6u4uHly-cCNcdEZdXfbIRWA
+
 git clone git@github.com:DenisDenis9331/my_bot_ruby.git
 
 cd my_bot_ruby
