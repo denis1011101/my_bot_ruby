171e66083b88d21fd658cdcdbf57185eb9debb5e
denis <denisdenis9331@gmail.com>
2024-10-26 11:05:22 +0500

    fix: some fix

--- bot.rb
+++ bot.rb
@@ -32,7 +32,7 @@ def start_send_telegram_message(telegram_bot, message_formatter, yaml_manager)
 end
 
 def valid_send_time?
-  TIME_NOW.hour.between?(11, 23) && TIME_NOW.min == 04
+  TIME_NOW.hour.between?(11, 23) && TIME_NOW.min == 30
 end
 
 listener(birthday_checker, telegram_bot, command_processor)
